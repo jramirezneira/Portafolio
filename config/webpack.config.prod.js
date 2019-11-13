bb0a684c476261bd0b166ab7b5b107c9a563859d
unknown <jramirezneira@gmail.com>
2019-11-13 22:08:11 +0100

    make it better

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -304,19 +304,21 @@ module.exports = {
     }),*/
 	
 	new UglifyJsPlugin({
-        test: /\.js($|\?)/i,
-        sourceMap: true,
-        uglifyOptions: {
-          mangle: {
-            keep_fnames: true,
-          },
-          compress: {
-            warnings: false,
-          },
-          output: {
-            beautify: false,
-          },
-        },
+         compress: {
+			warnings: false,
+			// Disabled because of an issue with Uglify breaking seemingly valid code:
+			// https://github.com/facebookincubator/create-react-app/issues/2376
+			// Pending further investigation:
+			// https://github.com/mishoo/UglifyJS2/issues/2011
+			comparisons: false,
+		  },
+		  output: {
+			comments: false,
+			// Turned on because emoji and regex is not minified properly using default
+			// https://github.com/facebookincubator/create-react-app/issues/2488
+			ascii_only: true,
+		  },
+		  sourceMap: shouldUseSourceMap,
       }),
 	
 	
